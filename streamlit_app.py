9cfa4a59f3db63da64a5fbd7b8f4312ebfad7d4d
Alonso <70250204+Alonsovn@users.noreply.github.com>
2022-05-12 19:13:02 -0600

    Add information option

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,9 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruityvice_reponse = requests.get('https://www.fruityvice.com/api/fruit/watermelon')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered',fruit_choice)
+fruityvice_reponse = requests.get('https://www.fruityvice.com/api/fruit/' + fruit_choice)
 
 
 #take the json version of response and normalize it
@@ -35,4 +37,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_reponse.json())
 
 #output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
+
    
