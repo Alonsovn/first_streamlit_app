c91f72faf8f9c186fbf94dbfc2502196df693aba
Alonso <70250204+Alonsovn@users.noreply.github.com>
2022-05-13 11:48:33 -0600

    Adding function get fruityvice response

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,17 +26,22 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+
+def get_fruityvice_data(fruit_choice):
+   fruityvice_reponse = requests.get('https://www.fruityvice.com/api/fruit/' + fruit_choice)
+   fruityvice_normalized = pandas.json_normalize(fruityvice_reponse.json())
+   return fruityvice_normalized 
+   
+
 # New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-   fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
       streamlit.error("Please select a fruit to get information")
-   else: 
-      streamlit.write('The user entered',fruit_choice)
-      fruityvice_reponse = requests.get('https://www.fruityvice.com/api/fruit/' + fruit_choice)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_reponse.json())
-      streamlit.dataframe(fruityvice_normalized)
+   else:
+      fruityvice_reponse_json = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(fruityvice_reponse_json)
 
 except URLError as e:
    streamlit.error()
