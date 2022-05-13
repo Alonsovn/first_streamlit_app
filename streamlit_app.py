54220add70bacf0a8c87f48a58570436507b01be
Alonso <70250204+Alonsovn@users.noreply.github.com>
2022-05-12 19:06:04 -0600

    Show the response data from fruityvice api on table

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,5 +28,11 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_reponse = requests.get('https://www.fruityvice.com/api/fruit/watermelon')
-streamlit.text(fruityvice_reponse.json())
+
+
+#take the json version of response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_reponse.json())
+
+#output it the screen as a table
+streamlit.dataframe(fruityvice_normalized)
    
